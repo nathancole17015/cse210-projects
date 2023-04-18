7037a92c6e1336e0fb7867baeb66ca6cc933d093
Nathan Cole <natemcole98@gmial.com>
2023-04-18 13:13:02 -0600

    finished HW

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string gradePercent = Console.ReadLine();
+        int grade = int.Parse(gradePercent);
+        if (grade >= 90){
+            Console.WriteLine("Your grade is a A");
+        }
+        else if (grade >= 80 && grade < 90){
+            Console.WriteLine("Your grade is a B");
+        }
+        else if (grade >= 70 && grade < 80){
+            Console.WriteLine("Your grade is a C");
+        }
+        else if (grade >= 60 && grade < 70){
+            Console.WriteLine("Your grade is a D");
+        }
+        else if (grade < 90){
+            Console.WriteLine("Your grade is a F");
+        }
     }
 }
