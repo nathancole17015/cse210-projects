5f977e55c033babd14a639f05f4b38839eb59e9f
Nathan Cole <natemcole98@gmail.com>
2023-04-24 16:06:49 -0600

    finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+
+
+        
+        int magicNumber;
+        Random randomGenerator = new Random();
+         magicNumber = randomGenerator.Next(1, 100);;
+         
+
+           int guess = -1;
+         
+         while (guess != magicNumber){
+             
+             Console.Write("What is your guess? ");
+             guess = int.Parse(Console.ReadLine());
+
+            if(guess < magicNumber){
+                Console.WriteLine("guess is too low!");
+            }
+
+            else if(guess > magicNumber ){
+
+                Console.WriteLine("guess is too high!");
+
+            }
+
+         }
+         Console.Write("You guessed it!");
+        
+
+        
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int numberInput = -1;
+        
+        while(numberInput != 0 ){
+
+        Console.Write("Input a number: ");
+        numberInput = int.Parse(Console.ReadLine());
+        numbers.Add(numberInput);
+
+        }
+        int sum = 0;
+        foreach (int num in numbers){
+            sum += sum=num;
+        }
+
+        Console.WriteLine($"The sum is {sum}");
+
+        float average = 0;
+        foreach(int num in numbers){
+            average = ((float)sum) / numbers.Count;
+        }
+        Console.WriteLine($"The average is {average}");
+
+        int max = numbers[0];
+        foreach(int num in numbers){
+            if(max > num){
+               max = num; 
+            }
+    
+        }
+
+        Console.WriteLine($"The max is {max}");
     }
-}
+}       
