e48737d6409ea8f3f43dcda4ff83225889d9237f
Nathan Cole <natemcole98@gmail.com>
2023-05-01 11:21:22 -0600

    changes have done

---
+++ abstract-example/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Book mistborn = new Book("Mistborn", "Brandon Sanderson");
+
+Book DiaryOfaWimpyKid = new Book("Diary of a Wimpy Kid", "Jeff Kinney");
+
+
+mistborn.Display();
+
+DiaryOfaWimpyKid.Display();
+
+Console.WriteLine(mistborn.IsAvailable());
---
+++ abstract-example/book.cs
@@ -0,0 +1,26 @@
+public class Book 
+{
+
+    private string _author;
+    private string _name;
+    private int _timesRead = 0;
+    private bool _available = false;
+
+    public Book(string name, string author){
+
+         _name = name;
+         _author = author;
+
+    }
+
+    public void Display(){
+        Console.WriteLine($"{_name} by {_author}");
+        if (!_available){
+            Console.WriteLine("[Checked out]");
+        }
+    }
+
+    public bool IsAvailable(){
+        return _available;
+    }
+}
