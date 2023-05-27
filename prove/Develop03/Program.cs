bfe9e3895296b9ae96e71c2df3b1646f4dc1f853
Nathan Cole <natemcole98@gmail.com>
2023-05-27 16:00:29 -0600

    finished scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,7 +1,7 @@
 
     // ce script = new Reference("John",3,3,4);
     
-    Scripture scripture = new Scripture(new Reference("john",3,16,16) ,"For God so loved the world, that He gave His only begotten Son, that whoever believes in Him shall not perish, but have eternal life");
+    Scripture scripture = new Scripture(new Reference("John",3,16,16) ,"For God so loved the world, that He gave His only begotten Son, that whoever believes in Him shall not perish, but have eternal life");
     scripture.Display();
    
       
