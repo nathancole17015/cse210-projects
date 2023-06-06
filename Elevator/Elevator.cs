4b682afaa55b3540df40d7a048ca24937412da14
Nathan Cole <natemcole98@gmail.com>
2023-06-06 09:42:39 -0600

    in class

---
+++ Elevator/Elevator.cs
@@ -0,0 +1,15 @@
+public class Elevator{
+
+    public bool _doorState = true ;
+
+
+    public void CloseDoor (){
+        _doorState = false;
+    }
+    public void OpenDoor (){
+        _doorState = true;
+    }
+    public void display(){
+        
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Reference reference = new Reference("John", 3, "16");
+
+        string words = "god gave his only begotten son that whoever";
+
+        Scripture scripture = new Scripture(reference, words);
+        scripture.Memorize();
+    }
+}
