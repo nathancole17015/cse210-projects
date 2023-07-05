9ba97dbf8a4fd4953ccaab38a42262b46add065a
Nathan Cole <natemcole98@gmail.com>
2023-07-05 12:18:47 -0600

    finished foundations 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -8,6 +8,7 @@ Lectures _lectures = new Lectures("Lecture","The Power of Positive Thinking: Unl
 Receptions _reception = new Receptions("Reception","Harmony and Celebration: An Evening of Cultural Fusion", " Immerse yourself in a captivating blend of diverse cultures through music, dance, and cuisine, \nas we come together to celebrate unity and the beauty of cultural diversity at this enchanting reception.", "November 10, 2023","7:00 PM","culturalfusionreception@gmail.com", _ReceptionAdd);
 
 
+Console.WriteLine("======================================");
 Console.WriteLine();
 Console.WriteLine("Standard Details");
 _reception.StandardInformation();
@@ -15,6 +16,7 @@ Console.WriteLine();
 _reception.FullDetails();
 Console.WriteLine();
 _reception.ShortDescription();
+Console.WriteLine("======================================");
 Console.WriteLine();
 Console.WriteLine("Standard Details");
 _lectures.StandardInformation();
@@ -22,6 +24,8 @@ Console.WriteLine();
 _lectures.FullDetails();
 Console.WriteLine();
 _lectures.ShortDescription();
+
+Console.WriteLine("======================================");
 Console.WriteLine();
 Console.WriteLine("Standard Details");
 _outdoorGathering.StandardInformation();
@@ -29,3 +33,4 @@ Console.WriteLine();
 _outdoorGathering.FullDetails();
 Console.WriteLine();
 _outdoorGathering.ShortDescription();
+Console.WriteLine("======================================");
