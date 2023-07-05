05fd2e5add568351fc5b56c9079c6a6a2fee7d72
Nathan Cole <natemcole98@gmail.com>
2023-07-05 11:45:11 -0600

    finished foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,7 +5,7 @@ Running _run = new Running("03 Nov 2022", "Running",30,10);
 
 Swimming _swim = new Swimming("20 Aug 2021","Swimming",40, 50);
 
-Cycling _cycling = new Cycling("14 May 2023", "Cycling",40,16);
+Cycling _cycling = new Cycling("14 May 2023", "Cycling",40,15);
 
 _activities.Add(_cycling);
 _activities.Add(_run);
